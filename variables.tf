b6138415ea69c65088758dc30046237c1e038ab6
Voxys <50014072+Voxys@users.noreply.github.com>
2023-01-22 16:59:10 -0500

    Addition to variable descriptions

--- variables.tf
+++ variables.tf
@@ -5,27 +5,27 @@ variable "subscription_id" {
 
 variable "location" {
   type        = string
-  description = "Azure location name used"
+  description = "Azure location name used (input example: Canada East)"
 }
 
 variable "vn_address_space" {
   type        = list(string)
-  description = "Virtual Network address space"
+  description = "Virtual Network address space (input example: 172.16.0.0/16)"
 }
 
 variable "vn_subnet" {
   type        = list(string)
-  description = "Subnet used by the Azure Virtual Network"
+  description = "Subnet used by the Azure Virtual Network (input example: 172.16.1.0/24)"
 }
 
 variable "lng_gateway_adress" {
   type        = string
-  description = "WAN interface IP address of your firewall"
+  description = "Public IP address of your firewall"
 }
 
 variable "lng_address_space" {
   type        = list(string)
-  description = "Local Network Gateway address space (IP range you use in your local network)"
+  description = "Local Network Gateway address space aka IP range you use in your local network (input example: 10.0.0.0/8)"
 }
 
 variable "shared_key" {
